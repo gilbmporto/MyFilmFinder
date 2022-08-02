7689e15e90c4c19e0ac314a2be37fe9683299542
Gil BMP <94876642+gilbmporto@users.noreply.github.com>
2022-08-01 22:20:51 -0300

    First Commit of FilmFinder Project
    Hope it works this time. 😨

---
+++ script.js
@@ -0,0 +1,184 @@
+//Define main stuff so the app goes on:
+const tmdbKey = 'f0f803eebc678edb9aa85f3128210ca5';
+const tmdbBaseUrl = 'https://api.themoviedb.org/3';
+const playBtn = document.getElementById('playBtn');
+
+const getGenres = async () => {
+    const genreRequestEndpoint = '/genre/movie/list';
+    let requestParams = `?api_key=${tmdbKey}`;
+    let urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
+
+    try {
+        const response = await fetch(urlToFetch, {cache: 'no-cache'});
+            if (response.ok) {
+                const jsonResponse = await response.json();
+                console.log(jsonResponse);
+                const genres = jsonResponse.genres;
+                console.log(genres);
+                return genres
+        }
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const getMovies = async () => {
+    const selectedGenre = getSelectedGenre();
+    const discoverMovieEndpoint = '/discover/movie';
+    const requestParams = `?api_key=${tmdbKey}&with_genres=${selectedGenre}`;
+    const urlToFetch = `${tmdbBaseUrl}${discoverMovieEndpoint}${requestParams}`;
+
+    try {
+            const response = await fetch(urlToFetch, {cache: 'no-cache'});
+            if (response.ok) {
+            const jsonResponse = await response.json();
+            console.log(jsonResponse);
+            let movies = jsonResponse.results;
+            console.log(movies);
+            return movies;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    };
+
+const getMovieInfo = async (movie) => {
+    let movieId = movie["id"];
+    const movieEndpoint = `/movie/${movieId}`;
+    const requestParams = `?api_key=${tmdbKey}`;
+    const urlToFetch = `${tmdbBaseUrl}${movieEndpoint}${requestParams}`;
+
+    try {
+            const response = await fetch(urlToFetch, {cache: 'no-cache'});
+            if (response.ok) {
+            let movieInfo = await response.json();
+            console.log(movieInfo);
+            return movieInfo;
+        }
+    } catch (err) {
+            console.log(err);
+    }
+    };
+
+// Gets a list of movies and ultimately displays the info of a random movie from the list
+const showRandomMovie = async () => {
+    const movieInfo = document.getElementById('movieInfo');
+    if (movieInfo.childNodes.length > 0) {
+        clearCurrentMovie();
+    };
+        let movies = await getMovies();
+        console.log(movies);
+        let randomMovie = getRandomMovie(movies);
+        console.log(randomMovie);
+        let info = await getMovieInfo(randomMovie);
+        console.log(info);
+        displayMovie(info);
+};
+
+//Let's get it started here:
+getGenres().then(populateGenreDropdown);
+playBtn.onclick = showRandomMovie;
+
+//Helper Functions:
+
+// Populate dropdown menu with all the available genres
+const populateGenreDropdown = (genres) => {
+    const select = document.getElementById('genres')
+
+    for (const genre of genres) {
+        let option = document.createElement("option");
+        option.value = genre.id;
+        option.text = genre.name;
+        select.appendChild(option);
+    }
+};
+
+// Returns the current genre selection from the dropdown menu
+const getSelectedGenre = () => {
+    const selectedGenre = document.getElementById('genres').value;
+    return selectedGenre;
+};
+
+// Displays the like and dislike buttons on the page
+const showBtns = () => {
+    const btnDiv = document.getElementById('likeOrDislikeBtns');
+    btnDiv.removeAttribute('hidden');
+};
+
+// Clear the current movie from the screen
+const clearCurrentMovie = () => {
+    const moviePosterDiv = document.getElementById('moviePoster');
+    const movieTextDiv = document.getElementById('movieText');
+    moviePosterDiv.innerHTML = '';
+    movieTextDiv.innerHTML = '';
+}
+
+// After liking a movie, clears the current movie from the screen and gets another random movie
+const likeMovie = () => {
+    clearCurrentMovie();
+    showRandomMovie();
+};
+
+// After disliking a movie, clears the current movie from the screen and gets another random movie
+const dislikeMovie = () => {
+    clearCurrentMovie();
+    showRandomMovie();
+};
+
+// Create HTML for movie poster
+const createMoviePoster = (posterPath) => {
+    const moviePosterUrl = `https://image.tmdb.org/t/p/original/${posterPath}`;
+
+    const posterImg = document.createElement('img');
+    posterImg.setAttribute('src', moviePosterUrl);
+    posterImg.setAttribute('id', 'moviePoster');
+  
+    return posterImg;
+};
+
+// Create HTML for movie title
+const createMovieTitle = (title) => {
+    const titleHeader = document.createElement('h1');
+    titleHeader.setAttribute('id', 'movieTitle');
+    titleHeader.innerHTML = title;
+  
+    return titleHeader;
+};
+
+// Create HTML for movie overview
+const createMovieOverview = (overview) => {
+    const overviewParagraph = document.createElement('p');
+    overviewParagraph.setAttribute('id', 'movieOverview');
+    overviewParagraph.innerHTML = overview;
+  
+    return overviewParagraph;
+};
+
+// Returns a random movie from the first page of movies
+const getRandomMovie = (movies) => {
+    const randomIndex = Math.floor(Math.random() * movies.length);
+    const randomMovie = movies[randomIndex];
+    return randomMovie;
+};
+
+// Uses the DOM to create HTML to display the movie
+const displayMovie = (movieInfo) => {
+    const moviePosterDiv = document.getElementById('moviePoster');
+    const movieTextDiv = document.getElementById('movieText');
+    const likeBtn = document.getElementById('likeBtn');
+    const dislikeBtn = document.getElementById('dislikeBtn');
+  
+    // Create HTML content containing movie info
+    const moviePoster = createMoviePoster(movieInfo.poster_path);
+    const titleHeader = createMovieTitle(movieInfo.title);
+    const overviewText = createMovieOverview(movieInfo.overview);
+  
+    // Append title, poster, and overview to page
+    moviePosterDiv.appendChild(moviePoster);
+    movieTextDiv.appendChild(titleHeader);
+    movieTextDiv.appendChild(overviewText);
+  
+    showBtns();
+    likeBtn.onclick = likeMovie;
+    dislikeBtn.onclick = dislikeMovie;
+};
